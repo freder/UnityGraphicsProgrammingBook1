1bca19caafb749927a1d59499bb9f9c7a32c5649
Frederic Brodbeck <der.froedi@gmail.com>
2021-01-30 14:38:32 +0100

    make sure images are displayed in a reasonable size

--- make-epub.js
+++ make-epub.js
@@ -17,3 +17,22 @@ execSync(cmd, { cwd: './articles' });
 // also create an html index page
 const html = files.map((f) => `<a href="${f}">${f}</a><br>`).join('\n');
 fs.writeFileSync('./articles/index.html', html);
+
+// and make sure images are displayed in a reasonable size
+files.forEach((f) => {
+	const filePath = `articles/${f}`;
+	let content = fs.readFileSync(filePath).toString();
+	content = content.replace(
+		'</head>', 
+		`
+			<style>
+				img {
+					max-width: 80vw;
+					max-height: 80vh;
+				}
+			</style>
+			</head>
+		`
+	);
+	fs.writeFileSync(filePath, content);
+})
